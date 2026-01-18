682b9646a04eacec8e9ed32decd5df4b06102417
sandrabudgarnisa <sandrabudigarnisa.09@gmail.com>
2026-01-18 23:17:14 +0700

    update3

--- Scripts/1. Verfy create new user/Script1768367542648.groovy
+++ Scripts/1. Verfy create new user/Script1768367542648.groovy
@@ -1,18 +1,19 @@
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import internal.GlobalVariable as GlobalVariable
-
 def uniqueName = "Sandra_" + System.currentTimeMillis()
 
+KeywordUtil.logInfo("PROFILE baseUrl = " + GlobalVariable.baseUrl)
+KeywordUtil.logInfo("Create user uniqueName = " + uniqueName)
+
 def response = WS.sendRequest(findTestObject('POST Create User', [
-	('name') : uniqueName
+  ('name') : uniqueName
 ]))
+
+KeywordUtil.logInfo("StatusCode = " + response.getStatusCode())
+KeywordUtil.logInfo("ResponseBody = " + response.getResponseText())
 
 WS.verifyResponseStatusCode(response, 201)
 
-// ambil id dari response dan simpan buat step berikutnya
+// ambil id
 def json = new groovy.json.JsonSlurper().parseText(response.getResponseText())
 def idVal = json?.id ?: json?.data?.id
-
 assert idVal != null : "ID tidak ditemukan di response (cek field id / data.id)"
 GlobalVariable.id = idVal.toString()
